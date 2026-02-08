7d6ee7276b2db0140f1845b86c5e910824300703
Matias Valdivieso <matiasvaldi18@gmail.com>
2026-02-08 14:36:11 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4994824897-767df",
+  "appId": "1:809219262327:web:4d7121cfc2123b5264c372",
+  "apiKey": "AIzaSyCicVY86SFVNs3YRL_AljyB5zWblm9OIOI",
+  "authDomain": "studio-4994824897-767df.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "809219262327"
+};
